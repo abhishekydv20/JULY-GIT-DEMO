674464235fea38eb5ee29f3add0c0d12f77ad33f
Abhishek Ydav  2008 <ay998713@gmail.com>
2024-08-31 13:46:28 +0530

    DAY 2 CLASS

---
+++ day2.py
@@ -0,0 +1,98 @@
+## variable
+# variable declares
+# variable name rule
+# int , float ,
+# data type
+## _________ string ________
+
+
+# st= "upflairs pvt ltd 301868  @ #25"
+# st = 'upflairs pvt ltd 301868 @ #25'
+st = """upflairs pvt ltd 301868 @ #25 """
+
+
+#print(st)
+#print(type(st))
+
+
+st ='upflairs'
+
+# primt(st[4:7])
+# print(st[:3])  # starting = 0
+# print(st[2:])  # stoping == end  
+
+# print(st[::1])
+# print(st[::2])    
+
+
+# st = 'upflairs'
+
+# print(st.upper())
+
+
+# st = 'UPFLAIRS'
+# print(st.lower())
+
+
+st = 'upflairs pvt ltd jaipur rajasthan'
+# print (len(st))
+# print(st.count('j'))
+
+# print(st.find('p'))
+
+# st = 'upflairs pvt ltd jaipur rajasthan'
+# print(st.replace('p',''))
+# print(st.endswith('
+
+# print(st.find('pvt'))
+# print(st[9:12])
+
+
+## data type ____boolean____
+
+# var1 = True
+# var2 = False
+# print(var1,var2)
+# print(type(var1),type(var2))
+
+
+## list
+# ls = [10,20,30,40,50.25,41,2.3,'upflairs',True]
+# print(ls)
+# print(type(ls))
+
+ls = [10,20,30,40,50,60,70]
+# print(ls[:2])
+
+# insert . deletion,
+student_name = ['mohit','rahul','abhi','ram',]
+print(student_name)
+print(type(student_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
